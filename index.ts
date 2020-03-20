fd175485db4b6783dfb412679d911d48a8dc20cd
Boris Vargas <borisvargaspaucara@gmail.com>
2020-03-20 00:32:01 -0400

    subiendo correcion

--- index.ts
+++ index.ts
@@ -3,22 +3,27 @@ let usuarios = [
         id: 1,
         nombre: 'Boris',
         edad: 23,
-        ci: '9884972',
+        ci: '111111',
     },
     {
         id: 2,
         nombre: 'Horacio',
         edad: 23,
-        ci: '1234567',
+        ci: '222222',
     }
 ];
 
 function findUser(id: number) {
+    let Noencontro = true;
     usuarios.forEach(usuario => {
         if (usuario.id === id) {
             console.log(`Hola ${usuario.nombre} tienes ${usuario.edad} y tu carnet es ${usuario.ci}`);
+            Noencontro = false;
         }
     });
+    if (Noencontro) console.log(`no se encontro usuario`);
 }
 
 findUser(1);
+findUser(2);
+findUser(100);
